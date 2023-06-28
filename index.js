d70cd6aeb488a1db01875dd17ed52b12e6edbbf3
KK-2495 <krishnankalimuthunadar@hotmail.com>
2023-06-28 11:58:39 +0530

    env update

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ import express from "express";
 import morgan from "morgan";
 import router from "./Route/routes.js";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/v1', router)
 
-mongoose.connect('mongodb+srv://Krish24:Krish%402495@cluster0.s8xz5ha.mongodb.net/Node_Test?retryWrites=true&w=majority')
+
+app.get("/ping", (req,res) => {
+    return res.send("Pong");
+})
+
+
+
+mongoose.connect(process.env.MONGODB_URL)
 .then(()=>console.log('DB-Connected'))
 .catch((err)=>console.log(err));
 
-app.listen(8000);
+app.listen(process.env.PORT);
